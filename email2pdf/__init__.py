56e1a426c0de125986bb2a6715d981c146af39dc
Jan Janssen <jan.janssen@outlook.com>
2022-02-13 10:23:58 -0700

    Create Python package

---
+++ email2pdf/__init__.py
@@ -0,0 +1,8 @@
+from email2pdf.email2pdf import \
+    get_unique_version, \
+    get_input_email, \
+    handle_message_body, \
+    remove_invalid_urls, \
+    get_formatted_header_info, \
+    output_body_pdf, \
+    FatalException
--- email2pdf/email2pdf.py
+++ email2pdf/email2pdf.py
@@ -755,34 +755,3 @@ def call_main(argv, syslog_handler, syserr_handler):
 
     if warning_pending and not mostly_hide_warnings:
         sys.exit(1)
-
-
-if __name__ == "__main__":
-    logger_setup = logging.getLogger("email2pdf")
-    logger_setup.propagate = False
-    logger_setup.setLevel(logging.DEBUG)
-
-    syserr_handler_setup = logging.StreamHandler(stream=sys.stderr)
-    syserr_handler_setup.setLevel(logging.WARNING)
-    syserr_formatter = logging.Formatter('%(levelname)s: %(message)s')
-    syserr_handler_setup.setFormatter(syserr_formatter)
-    logger_setup.addHandler(syserr_handler_setup)
-
-    if _platform == "linux" or _platform == "linux2":
-        SYSLOG_ADDRESS = '/dev/log'
-    elif _platform == "darwin":
-        SYSLOG_ADDRESS = '/var/run/syslog'
-    else:
-        logger_setup.warning("I don't know this platform (" + _platform + "); cannot log to syslog.")
-        SYSLOG_ADDRESS = None
-
-    if SYSLOG_ADDRESS and os.path.exists(SYSLOG_ADDRESS):
-        syslog_handler_setup = logging.handlers.SysLogHandler(address=SYSLOG_ADDRESS)
-        syslog_handler_setup.setLevel(logging.INFO)
-        SYSLOG_FORMATTER = logging.Formatter('%(pathname)s[%(process)d] %(levelname)s %(lineno)d %(message)s')
-        syslog_handler_setup.setFormatter(SYSLOG_FORMATTER)
-        logger_setup.addHandler(syslog_handler_setup)
-    else:
-        syslog_handler_setup = None
-
-    call_main(sys.argv, syslog_handler_setup, syserr_handler_setup)
