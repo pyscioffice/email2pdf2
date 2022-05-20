6af243c92c109a800cf653fbae1368e470f2d4db
Stephan Geulette <s.geulette@imio.be>
2022-05-20 10:52:51 +0200

    Improved comment

--- email2pdf2/email2pdf2.py
+++ email2pdf2/email2pdf2.py
@@ -485,7 +485,7 @@ def can_url_fetch(src):
     try:
         encoded_src = src.replace(" ", "%20")
         req = Request(encoded_src)
-        urlopen(req, timeout=10)  # 20220111 added timeout
+        urlopen(req, timeout=10)  # Reduced timeout to avoid long try on broken external link
     except HTTPError:
         return False
     except URLError:
